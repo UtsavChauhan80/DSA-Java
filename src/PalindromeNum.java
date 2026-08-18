c14cc4841d914c297db4b833dc77242b9bdbdb20
Utsav Chauhan <utsavchauhan507@gmail.com>
2026-08-18 21:56:39 +0530

    Add Palindrome number program

---
+++ src/PalindromeNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PalindromeNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int original = n;
+        int reverse = 0;
+        while (n > 0) {
+            reverse = reverse * 10 + n % 10;
+            n = n / 10;
+        }
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
