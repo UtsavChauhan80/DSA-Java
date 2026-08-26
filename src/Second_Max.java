cb63cdea9ddf9e866ee1b9cc683de2055d4521f7
Utsav Chauhan <utsavchauhan507@gmail.com>
2026-08-26 21:22:30 +0530

    Add Second Max program.

---
+++ src/Second_Max.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Second_Max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+        System.out.println("Second maximum = " + secondMax);
+    }
+}
