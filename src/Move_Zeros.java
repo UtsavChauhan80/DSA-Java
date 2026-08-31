1cc59fbf7548064392210bd57dbfcc26ec4fefd7
Utsav Chauhan <utsavchauhan507@gmail.com>
2026-08-31 23:18:37 +0530

    Add Move Zeros program.

---
+++ src/Move_Zeros.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Move_Zeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.println("Array after moving zeroes:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
