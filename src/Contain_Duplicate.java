d21a408d03f41fa7c10c80e64ca111003abc92f3
Utsav Chauhan <utsavchauhan507@gmail.com>
2026-09-03 22:36:13 +0530

    Add Contains Duplicate program.

---
+++ src/Contain_Duplicate.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contain_Duplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println("true");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("false");
+    }
+}
